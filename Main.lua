11f0a0fa5dcb45b7b86bd446682247f26cd7304f
ossfr <zangurad@gmail.com>
2024-10-09 18:27:17 +0400

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -10,30 +10,18 @@ local scripts = {
     [13772394625] = "https://raw.githubusercontent.com/ossfr/BladeBall/refs/heads/main/Freemium.lua"
 }
 
-if placeId == 6516141723 then
-    local success, result = pcall(function()
-        return loadstring(game:HttpGet(scripts[6516141723]))()
-    end)
+local scriptUrl = scripts[placeId]
 
+if scriptUrl then
+    local success, result = pcall(function()
+        return loadstring(game:HttpGet(scriptUrl))()
+    end)
+    
     if success then
-        print("Doors script loaded successfully!")
+        print("Script loaded successfully!")
     else
-        warn("Failed to load Doors script: " .. tostring(result))
+        warn("Failed to load script: " .. tostring(result))
     end
 else
-    local scriptUrl = scripts[placeId]
-
-    if scriptUrl then
-        local success, result = pcall(function()
-            return loadstring(game:HttpGet(scriptUrl))()
-        end)
-        
-        if success then
-            print("Script loaded successfully!")
-        else
-            warn("Failed to load script: " .. tostring(result))
-        end
-    else
-        warn("No script found for this game place ID.")
-    end
+    warn("No script found for this game place ID.")
 end
